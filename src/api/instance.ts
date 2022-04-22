a47f7a718f1f7fff058757c8615145d930e3808d
Alex Ilin <alexander.ilyin3@gmail.com>
2022-04-22 23:45:20 +0300

    added enviroment token

--- src/api/instance.ts
+++ src/api/instance.ts
@@ -1,11 +1,13 @@
 import axios from 'axios'
 
+const token = process.env.REACT_APP_BEARER_TOKEN
+
 export const instance = axios.create({
   baseURL: 'https://api.github.com/',
-  headers: { 'Authorization': 'Bearer '+ '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' }
+  headers: { 'Authorization': 'Bearer ' + token }
 })
 
 export const graphQlInstance = axios.create({
   baseURL: 'https://api.github.com/graphql',
-  headers: { 'Authorization': 'Bearer '+ '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' }
+  headers: { 'Authorization': 'Bearer ' + token }
 })
